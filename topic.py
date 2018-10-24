109951881786129b28e5e559ddf9e37061053f26
Shlomi Fish <shlomif@shlomifish.org>
2018-10-24 20:35:19 +0300

    flake8.

--- topic.py
+++ topic.py
@@ -1,9 +1,13 @@
-#(c) Xavier Combelle here made in public domain
+#!/usr/bin/env python3
+# (c) Xavier Combelle here made in public domain
 
-import subprocess,re
+import re
+
 for line in open("FAQ.mdwn"):
     if line.startswith("#"):
-        print("""- [{}](#{})""".format(line.strip("#").strip(),re.sub("\W+","-",line.strip("#").strip()).strip("-")))
-        
+        print("""- [{}](#{})""".format(
+            line.strip("#").strip(),
+            re.sub("\W+", "-", line.strip("#").strip()).strip("-")))
+
 for line in open("FAQ.mdwn"):
-    print(line,end="")
+    print(line, end="")
