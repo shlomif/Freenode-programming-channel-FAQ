64d4fe11b52a44f2cd1e21dd5848a1debdbd0e0e
Shlomi Fish <shlomif@shlomifish.org>
2019-01-23 11:46:17 +0200

    fix for links

--- topic.py
+++ topic.py
@@ -3,15 +3,21 @@
 
 import re
 
+# * https://github.com/jch/html-pipeline/
+# blob/master/lib/html/pipeline/toc_filter.rb
+
 
 def add_toc(fn):
     out_s = ""
     for line in open(fn):
         if line.startswith("#"):
+            # See:
+            # * https://gist.github.com/asabaylus/3071099
             stripped = line.strip("#").strip()
             out_s += ("""- [{}](#{})\n""".format(
                 stripped,
-                re.sub("\\W+", "-", stripped).strip("-")))
+                re.sub("[ ]", "-",
+                       re.sub("[^\\w\\- ]", "", stripped.lower())).strip("-")))
 
     out_s += open(fn).read()
     return out_s
