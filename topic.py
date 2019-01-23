971f7d41ca60b169ca3a75e9556ab297100dfd96
Shlomi Fish <shlomif@shlomifish.org>
2019-01-23 11:25:52 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- topic.py
+++ topic.py
@@ -8,9 +8,10 @@ def add_toc(fn):
     out_s = ""
     for line in open(fn):
         if line.startswith("#"):
+            stripped = line.strip("#").strip()
             out_s += ("""- [{}](#{})\n""".format(
-                line.strip("#").strip(),
-                re.sub("\\W+", "-", line.strip("#").strip()).strip("-")))
+                stripped,
+                re.sub("\\W+", "-", stripped).strip("-")))
 
     out_s += open(fn).read()
     return out_s
