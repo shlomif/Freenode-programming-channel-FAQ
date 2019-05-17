ce6e49a34d7a674bc91f593979415e0b3cbed716
Shlomi Fish <shlomif@shlomifish.org>
2019-05-17 11:41:19 +0300

    convert to argparse.

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 GEN_FAQ = FAQ_with_ToC__generated.md
+SRC_FAQ = FAQ.mdwn
 GEN = topic.py
 
 all: $(GEN_FAQ)
 
 $(GEN_FAQ): FAQ.mdwn $(GEN)
-	python3 $(GEN) $@
+	python3 $(GEN) --input $< --output $@
--- topic.py
+++ topic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # (c) Xavier Combelle here made in public domain
 
+import argparse
 import re
 import sys
 
@@ -24,4 +25,16 @@ def add_toc(fn):
     return out_s
 
 
-open(sys.argv[1], "wt").write(add_toc("FAQ.mdwn"))
+def main(argv):
+    parser = argparse.ArgumentParser(
+        prog='PROG',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--output', type=str, required=True,
+                        help='output filename')
+    parser.add_argument('--input', type=str, required=True,
+                        help='Input filename')
+    args = parser.parse_args(argv[1:])
+    open(args.output, "wt").write(add_toc(args.input))
+
+
+main(sys.argv)
