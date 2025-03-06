928e529f46061768ad18c826b905a72b239dc99d
Shlomi Fish <shlomif@shlomifish.org>
2025-03-06 15:36:59 +0200

    simplify bin/my-cookiecutter.pl

--- bin/my-cookiecutter.pl
+++ bin/my-cookiecutter.pl
@@ -6,25 +6,11 @@ use autodie;
 use lib './lib';
 use Shlomif::MySystem qw/ my_system /;
 
-my $src_dn =
-"$ENV{HOME}/.cookiecutters/cookiecutter--shlomif-latemp-sites/\{\{cookiecutter.project_slug\}\}";
-
-# if ( -d $src_dn )
-if (0)
-{
-    my_system( [ "rsync", "-ra", "$src_dn/", "./" ], "rsync failed", );
-    require Path::Tiny;
-    Path::Tiny::path("./bin/batch-inplace-html-minifier")
-        ->edit_raw( sub { s/\n\{% (?:end)?raw %\}\n/\n/g; } );
-}
-else
-{
-    my_system(
-        [
-            'cookiecutter', '-f', '--no-input',
-            'gh:shlomif/cookiecutter--shlomif-latemp-sites',
-            'project_slug=.',
-        ],
-        'cookiecutter failed.'
-    );
-}
+my_system(
+    [
+        'cookiecutter', '-f', '--no-input',
+        'gh:shlomif/cookiecutter--shlomif-latemp-sites',
+        'project_slug=.',
+    ],
+    'cookiecutter failed.'
+);
